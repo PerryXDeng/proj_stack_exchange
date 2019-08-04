b6572518edf9e190d7825cff2a1108bb3ba3bac5
PerryXDeng <pxd256@protonmail.com>
2019-08-04 10:01:18 -0400

    fixed chronological arithmetic

--- analyses.py
+++ analyses.py
@@ -312,7 +312,7 @@ def main():
 
   # start_date = get_first_post_time(sql_context).collect()[0]['dateCreated'].replace(minute=0, second=0)
   end_date = get_last_post_time(sql_context).collect()[0]['dateCreated'].replace(minute=0, second=0)
-  start_date = end_date - relativedelta(years=-1)
+  start_date = end_date + relativedelta(years=-1)
   index_hourly_total_post_sizes(spark_session, sql_context, start_date, end_date)
 
 
